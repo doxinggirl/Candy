61fe4d3f1e176c0ea8848be74b99286b992d3d90
why you do it?!!!!!!! github!!!!!!! <nxiao4956@gmail.com>
2025-06-05 10:51:40 +0900

    logger

---
+++ builder/utils/module/logger.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+import getpass
+from colorama import Fore, init
+
+init(autoreset=True)  
+
+def timestamp():
+    now = datetime.now()
+    return f"{Fore.CYAN}[{now.hour}:{now.minute}:{now.second}]{Fore.RESET} "
+
+def log_debug(message):
+    username = getpass.getuser()
+    print(f"{timestamp()}{Fore.GREEN}DEBUG {Fore.LIGHTBLACK_EX}(User: {username}) {Fore.RESET}{message}")
