fac8400a3b71ab0f15ff5ee15df697edd76d2b5c
addadi000 <nxiao4956@gmail.com>
2025-06-06 17:20:52 +0900

    new log function

--- builder/utils/module/logger.py
+++ builder/utils/module/logger.py
@@ -11,3 +11,12 @@ def timestamp():
 def log_debug(message):
     username = getpass.getuser()
     print(f"{timestamp()}{Fore.GREEN}DEBUG {Fore.LIGHTBLACK_EX}(User: {username}) {Fore.RESET}{message}")
+
+def log_error(message):
+    username = getpass.getuser()
+    print(f"{timestamp()}{Fore.RED}ERROR {Fore.LIGHTBLACK_EX}(User: {username}) {Fore.RESET}{Fore.RED}{message}")
+
+def log_warn(message):
+    username = getpass.getuser()
+    print(f"{timestamp()}{Fore.YELLOW}WARNING {Fore.LIGHTBLACK_EX}(User: {username}) {Fore.RESET}{message}")
+
